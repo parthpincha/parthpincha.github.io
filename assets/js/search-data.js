85973d46e7417796d4a35c96b75fe2b3d2e272b5
parthpincha <parthpincha@gmail.com>
2026-02-10 18:25:16 +0000

    Deploying to gh-pages from @ parthpincha/parthpincha.github.io@563d3df5262853a945edee24960ea09b887c9fb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-exploring-game-theory-applications-in-ai-specifically-focusing-on-poker-theory-and-decision-making-mathematics",
+          title: 'Exploring Game Theory applications in AI, specifically focusing on Poker Theory and decision-making...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
